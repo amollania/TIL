1ee2b63a810efdde46ed21f9d6e46bb8dfb11c96
amollania <eyestm10@gmail.com>
2020-01-18 19:03:51 +0900

    java conditional

---
+++ java/conditional.java
@@ -0,0 +1,22 @@
+//java条件分岐
+//if文の例
+
+class Main{
+    public static void main(String[] args) {
+        if (true) {
+            System.out.println("This is TRUE");
+        }
+
+        if (false) {
+            System.out.println("This is FALSE");
+        }
+
+        int x = 5;
+        int y = 10;
+
+        if (x < y) {
+            System.out.println(x+"は"+y+"より小さい");
+            System.out.println("合計は"+(x+y)+"です。");
+        }
+    }
+}

667783385527e8e7e9a8331402714cb27fbff85f
amollania <eyestm10@gmail.com>
2020-03-12 00:06:23 +0900

    2020-03-12

---
+++ java/lecture04/ArrayCreateByValueListExqample1.java
@@ -0,0 +1,19 @@
+package lecture04;
+
+public class ArrayCreateByValueListExqample1 {
+	public static void main(String[] args) {
+		int[] scores = {83, 90, 87};
+		
+		System.out.println("score[0] : " + scores[0]);
+		System.out.println("score[1] : " + scores[1]);
+		System.out.println("score[2] : " + scores[2]);
+		
+		int sum = 0;
+		for(int i=0; i<3; i++) {
+			sum += scores[i];
+		}
+		System.out.println("Total : " + sum);
+		double avg = (double) sum / 3;
+		System.out.println("Avg : " + avg);
+	}
+}
